10073235febad6010051f282858a9fb6a4c1e20b
Olha Starukh <olga83.starukh@gmail.com>
2026-05-05 01:11:21 +0300

    hw7

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categoriesItems = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categoriesItems.length}`);
+categoriesItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const elementsCount = item.querySelectorAll("li").length;
+  console.log(`Category:${categoryTitle}`);
+  console.log(`Elements: ${elementsCount}`);
+});
---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+  }
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(formData);
+  event.currentTarget.reset();
+});
