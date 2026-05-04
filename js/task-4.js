29200636e52b4c8f6c83fa6a74a302958b9487c0
Olha Starukh <olga83.starukh@gmail.com>
2026-05-05 01:19:20 +0300

    add return in task4

--- js/task-4.js
+++ js/task-4.js
@@ -8,6 +8,7 @@ loginForm.addEventListener("submit", (event) => {
   const passwordValue = password.value.trim();
   if (emailValue === "" || passwordValue === "") {
     alert("All form fields must be filled in");
+    return;
   }
   const formData = {
     email: emailValue,
